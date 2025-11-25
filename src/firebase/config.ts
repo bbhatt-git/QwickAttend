c7abd2667cc3903a0d6f304070fec44d7eb2d437
Bhupesh Bhatt <bbhatt09.np@gmail.com>
2025-11-25 16:52:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5124146837-580d6",
+  "appId": "1:27863074286:web:307317775b7901cf8a7aff",
+  "apiKey": "AIzaSyBEUfQBzzamu3tIZWh-xGDhGeQ_NXTp4To",
+  "authDomain": "studio-5124146837-580d6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "27863074286"
+};
