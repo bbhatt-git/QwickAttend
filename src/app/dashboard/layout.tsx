edc4d06fa1b2ca66bf8603ed8018e1cf6a538993
Bhupesh Bhatt <bbhatt09.np@gmail.com>
2025-11-26 16:25:53 +0000

    Make the footer appear at the very end of the screen

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -11,7 +11,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
       <MainSidebar />
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <Header />
-        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+        <main className="flex-1 p-4 sm:px-6 sm:py-0 md:gap-8">
           <Suspense fallback={<div className="flex justify-center items-center h-full"><Loader2 className="h-8 w-8 animate-spin" /></div>}>
             {children}
           </Suspense>
