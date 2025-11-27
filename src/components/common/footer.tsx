474a91c4603cc1375499c143f386c38b5cbb6690
Bhupesh Bhatt <bbhatt09.np@gmail.com>
2025-11-27 15:57:59 +0000

    Still same!!!

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <footer className="mt-auto border-t pt-2">
-      <div className="container mx-auto flex flex-col items-center justify-between gap-2 pb-2 text-center text-sm text-muted-foreground sm:flex-row">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-2 text-center text-sm text-muted-foreground sm:flex-row">
         <div className="flex items-center gap-1.5">
           Made with <Heart className="h-4 w-4 text-red-500 fill-current" /> by{' '}
           <Link href="https://github.com/bbhatt-git" target="_blank" rel="noopener noreferrer" className="font-medium transition-colors hover:text-primary">
