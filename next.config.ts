2919980c3f23062a2bc56b48841900a4eba8f3b7
Bhupesh Bhatt <bhattbhupesh.369@gmail.com>
2025-12-06 07:57:03 +0000

    solve then

--- next.config.ts
+++ next.config.ts
@@ -6,9 +6,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
@@ -30,7 +27,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https'
         hostname: 'firebasestorage.googleapis.com',
         port: '',
         pathname: '/**',
