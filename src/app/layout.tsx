bc2e3daaa3cd8fc42bfc6833fa9720b13fbf9a6e
Bhupesh Bhatt <bhattbhupesh.369@gmail.com>
2025-12-10 15:42:01 +0000

    Use the Light mode as default for all devices when opened newly or for f

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,7 @@ export default function RootLayout({
       <body className={`font-sans antialiased ${inter.variable}`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
           disableTransitionOnChange
         >
           <FirebaseClientProvider>
