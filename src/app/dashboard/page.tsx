b32269d797ada96924249d42b50049341d7a38e0
Bhupesh Bhatt <bhattbhupesh.369@gmail.com>
2025-12-11 16:34:16 +0000

    You fucking bitch I want it to be on the bottom even if there is no cont

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -75,7 +75,7 @@ export default function DashboardPage() {
   ]
 
   return (
-    <div className="flex flex-col h-full">
+    <div className="flex flex-col flex-1">
       <div className="flex-grow space-y-6">
         <WelcomeHeader />
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
